fa45f16133fd14ef54ee70a6c8cd288342d220ef
mstankai <mi.stankaityte@gmail.com>
2024-12-06 22:18:47 +0100

    gitignore and template

---
+++ 2024/template.py
@@ -0,0 +1,30 @@
+import re
+import sys
+import numpy as np
+
+def get_input(input_path):
+
+    # with open(input_path, 'r') as f:
+    #     text = f.read()
+    text = ""
+    return text
+
+# -----------------------------------------------
+def part_1(text):
+    # print(f"Part 1: {total}")
+    return
+# -----------------------------------------------
+def part_2(text):
+    # print(f"Part 2: {total}")
+    return
+
+# -----------------------------------------------
+def main():
+    input_path = './input/dec1_input.txt'
+    text = get_input(input_path)
+    part_1(text)
+    part_2(text)
+
+# -----------------------------------------------
+if __name__ == "__main__":
+    main()
