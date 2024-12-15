fe67591784eadc39ba3daf2878298a8665b3e4da
mstankai <mi.stankaityte@gmail.com>
2024-12-15 13:54:15 +0100

    2024.12.04 simplified pt2

--- 2024/dec4.py
+++ 2024/dec4.py
@@ -75,63 +75,35 @@ def part_1(text):
 
     print(f"Part 1: XMAS count = {n}")
     return
+
+
 # -----------------------------------------------
 def part_2(text):
 
     m = process_text(text)
-    r, c = m.shape
-    
-    # get a dummy array with indexes
-    m_dummy = []
-    for ir in range(r):
-        r_dummy = []
-        for ic in range(c):
-            r_dummy.append((ir*c)+ic)
-        m_dummy.append(r_dummy)
-    m_dummy = np.array(m_dummy)
+    n_rows, n_cols = m.shape
 
-    # get diagonals
-    md = get_diagonals(m)
-    mrd = get_diagonals(np.fliplr(m))
-
-    # get dummy diagonals
-    md_dummy = get_diagonals(m_dummy)
-    mrd_dummy = get_diagonals(np.fliplr(m_dummy))
-    
-    # all matrices to run over
-    matrices = [md, flip(md), mrd, flip(mrd)]
-    dummies = [md_dummy, flip(md_dummy), mrd_dummy, flip(mrd_dummy)]
-
-
-    # find indices of 'a's of 'MAS' foind in any of the matrices
-    a_true_indexes_all = []
-    for matrix, dummy in zip(matrices, dummies):
-        # get the positions of matches within each line
-        match_line_positions = search_matrix(matrix, 'MAS')[1]
-        # get the positions of 'A'
-        a_line_positions = [[i + 1 for i in l] for l in match_line_positions]
-
-        # get true indexes of the 'a's
-        a_true_indexes = []
-        for a_found_in_line, true_idx_line in zip(a_line_positions, dummy):
-            if len(a_found_in_line) == 0 : continue
-
-            for ia in a_found_in_line:
-                a_true_indexes.append(true_idx_line[ia])
-        
-        a_true_indexes_all.append(a_true_indexes)
-
-    # get the a indexes from different diagonal directions
-    a_true_lc = a_true_indexes_all[:2]
-    a_true_rc = a_true_indexes_all[2:]
-
-
-    # check for overlaping 'a's between the two directions
     n = 0
-    for lc in a_true_lc:
-        for rc in a_true_rc:
-            n += len(set(lc).intersection(set(rc)))
-    
+    for i in range(1, n_rows-1, 1):
+        for j in range(1, n_cols-1, 1):
+
+            c, tl, tr, bl, br = (
+                m[i][j],
+                m[i-1][j-1],
+                m[i-1][j+1],
+                m[i+1][j-1],
+                m[i+1][j+1],
+            )
+
+            if c  != 'A': continue
+
+            lr_mas = ((tl == 'M') & (br == 'S')) | ((tl == 'S') & (br == 'M'))
+            rl_mas = ((tr == 'M') & (bl == 'S')) | ((tr == 'S') & (bl == 'M'))
+
+
+            if lr_mas & rl_mas : 
+                n += 1
+
     print(f"Part 2: X-MAS count = {n}")
     return
 
