1e5cba9de4f2ea83eed7ff5356f62f64b44f6ee1
mstankai <mi.stankaityte@gmail.com>
2025-09-13 21:43:05 +0100

    2024.12.09: solved part 1, part 2 stuck with ordering

--- 2024/dec9/main.py
+++ 2024/dec9/main.py
@@ -3,6 +3,25 @@ import sys
 import numpy as np
 
 # -----------------------------------------------
+class Item:
+    def __init__(self, fid: int, l: int):
+        self.fid = fid
+        self.len = l
+        self.is_file = (fid >= 0)
+
+    def __str__(self):
+        if self.is_file:
+            return f"File - ID: {self.fid}, size: {self.len}"
+        else:
+            return f"Space - size: {self.len}"
+        
+    def viz(self):
+        if self.is_file:
+            return str(self.fid)*self.len
+        else:
+            return '.'*self.len
+
+# -----------------------------------------------
 def get_input(input_path):
 
     with open(input_path, 'r') as f:
@@ -12,25 +31,45 @@ def get_input(input_path):
     # text = "12345"
     return text
 
-# -----------------------------------------------
-def get_map_str(text: str):
 
-    fs = ''
+# -----------------------------------------------
+def get_map_p1(text: str):
+
+    filemap = []
 
     for i, c in enumerate(text):
         idx = str(i // 2)
         is_file = (i % 2 == 0)
         l = int(c)
+
         if is_file:
-            fs += idx*l
+            filemap.extend([idx for j in range(l)])
         else :
-            fs += '.'*l
+            filemap.extend(['.' for j in range(l)])
     
-    return fs
+    return filemap
 
 # -----------------------------------------------
-def part_1(m):
+def get_map_p2(text: str):
 
+    filemap = []
+
+    for i, c in enumerate(text):
+        idx = i // 2
+        is_file = (i % 2 == 0)
+        l = int(c)
+
+        if is_file:
+            filemap.append(Item(idx, l))
+        else :
+            filemap.append(Item(-1,l))
+    
+    return filemap
+
+# -----------------------------------------------
+def part_1(text):
+
+    m = get_map_p1(text)
 
     # --- rearrange files ---
     i, j = 0, len(m) - 1
@@ -50,7 +89,7 @@ def part_1(m):
         i += 1
         j -= 1
 
-        print("".join(m))
+        # print(" ".join(m))
 
     # --- get file checksum ---
     cs = 0
@@ -58,14 +97,66 @@ def part_1(m):
         if fid_str == '.': break
         fid = int(fid_str)
         cs += pos * fid
+        # print(pos, fid)
     
     print(f"Part 1, checksum: {cs}")
 
 # -----------------------------------------------
+def print_map2(m):
+    mp = [i.viz() for i in m]
+    print(" ".join(mp))
+# -----------------------------------------------
 def part_2(text):
 
-    # print("Input: ",text)
-    # print("Files: ", map_str)
+    print("Input: ",text)
+    m = get_map_p2(text)
+
+    # --- rearrange files ---
+    m_orig = m.copy()
+
+    j = len(m) - 1
+    jm = len(m) - 1
+
+    while j > 0:
+        b = m_orig[j]
+
+        if not b.is_file: 
+            j -= 1
+            jm -= 1
+            continue
+
+        i = 0
+        while i <= j: 
+            f = m[i]
+            if (f.is_file) or (f.len < b.len):
+                i += 1
+                continue
+
+            # --- move file ---
+            print_map2(m)
+
+            print("f: ", f)
+            print("b: ", b)
+            print("i,j,jm: ",i,j,jm)
+
+            m[jm] = Item(-1, b.len) # make space
+            
+            diff = f.len - b.len
+            if diff == 0:
+                m[i] = b
+            else:
+                m[i:i+1] = [b, Item(-1, diff)]
+                jm += 1
+            
+            j-=1
+            jm-=1
+
+            print_map2(m)
+            print("-----------")
+            break
+
+        j-=1
+        jm-=1
     # print(f"Part 2: {total}")
     return
 
@@ -73,12 +164,9 @@ def part_2(text):
 def main():
     input_path = './dec9/input.txt'
     text = get_input(input_path)
-    map_str = get_map_str(text)
-    m = list(map_str)
-    print("Filesystem size: ",len(m))
 
-    part_1(m)
-    # part_2(text)
+    # part_1(text)
+    part_2(text)
 
 # -----------------------------------------------
 if __name__ == "__main__":
